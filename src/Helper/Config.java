eb72464a3113cd35e366290d40ec53023e3726db
Furkan Çinçik <furkancincik1999@hotmail.com>
2024-06-14 22:56:37 +0300

    step-1.0
    -Steps += 0.2
    
    -Helper, Config sınfları oluşturuldu.
    -Veritabanı bağlantısı sağlandı.

---
+++ src/Helper/Config.java
@@ -0,0 +1,8 @@
+package Helper;
+
+public class Config {
+    public static final String PROJECT_TITLE = "Turizm Acente Sistemi";
+    public static final String DB_URL = "jdbc:postgresql://localhost:5432/TurizmAcenteDB";
+    public static final String DB_USERNAME = "postgres";
+    public static final String DB_PASSWORD = "Furkan123";
+}
---
+++ src/Helper/DBConnector.java
@@ -0,0 +1,25 @@
+package Helper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+    private Connection conn = null;
+
+    public Connection connectDB(){
+        try {
+            this.conn = DriverManager.getConnection(Config.DB_URL,Config.DB_USERNAME,Config.DB_PASSWORD);
+            System.out.println("Veritabanına bağlanıldı");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return this.conn;
+    }
+
+    public static Connection getInstance(){
+        DBConnector db =new DBConnector();
+        return db.connectDB();
+    }
+
+}
